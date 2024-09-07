00fcc7d3287112ffc073b485f60288eaf86f9e53
JuliLaplace <juli.laplace@gmail.com>
2024-09-07 11:40:15 -0300

    Se agregaron componentes Login - Sobre mi - Chat - Home. Se agrego diseño

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,45 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ChatComponent } from './componentes/chat/chat.component';
+import { PaginaErrorComponent } from './componentes/pagina-error/pagina-error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    
+      },
+      {
+        path: 'home',
+        redirectTo: '', pathMatch: "full",
+    
+      },
+      {
+        path: 'sobre-mi',
+        component: SobreMiComponent,
+    
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+    
+      },
+      {
+        path: 'registro',
+        component: RegistroComponent,
+    
+      },
+      {
+        path: 'chat',
+        component: ChatComponent,
+    
+      },
+      {
+        path: '**',
+        component: PaginaErrorComponent,
+    
+      }
+];
---
+++ src/app/componentes/login/login.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = "";
+  pwd: string = "";
+
+  login() {
+    
+  }
+
+ 
+
+  registroAutomatico(){
+    
+  }
+}
