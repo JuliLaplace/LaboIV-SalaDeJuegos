fcf2aa0bd740cba6a59c7f16dec8794b6ee603fe
JuliLaplace <juli.laplace@gmail.com>
2024-09-08 23:18:12 -0300

    Se agregaron estilos - componente registro

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
+  email: string = "";
+  pwd: string = "";
+  errorFlag: boolean = false;
+  errorMsj: string = "";
+  registrar() {
 
+  }
 }
