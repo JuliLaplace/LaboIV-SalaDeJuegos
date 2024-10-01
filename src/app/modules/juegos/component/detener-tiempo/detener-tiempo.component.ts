0b255f5883804c8929dd8ade88acfd79fba020af
JuliLaplace <juli.laplace@gmail.com>
2024-10-01 16:02:18 -0300

    4to sprint. Se agrego juego detener tiempo, se modificaron detalles de assets en juego ahorcado

--- src/app/modules/juegos/component/detener-tiempo/detener-tiempo.component.ts
+++ src/app/modules/juegos/component/detener-tiempo/detener-tiempo.component.ts
@@ -7,86 +7,77 @@ import { Component } from '@angular/core';
 })
 export class DetenerTiempoComponent {
 
-  personajes : any [] = [];
-  personajeParaAdivinar: any;
-  cantidadPersonajes: number = 826;
-
-  personajesUsados : number[] = [];
+ 
   vidasUsuario: number = 3;
   vidas: boolean[] = [true, true, true]; //las uso para mostrar los corazones rosas (true) o negros (false)
   juegoFinalizado: boolean = false;
   score: number = 0;
   mostrarResultado: boolean = false;
   mensajeResultado!: string;
-  
+  tiempoActual = 0;
+  usuarioJugando: boolean = false;
+  intervalo: any;
+  tiempoObjetivo: number = 5; // Tiempo objetivo en segundos
+  incrementoTiempoObjetivo: number = 5;
+  mostrarReglas: boolean = true;
 
 
   constructor() {
   }
 
-  ngOnInit(): void {
-   
-
-    
-  }
-  generarNumerosRandom(idUsados: number[]): number[] {
-    const numeros: number[] = [];
-    while (numeros.length < 4) {
-      const randomNum = Math.floor(Math.random() * (826 - 1 + 1)) + 1;
-  
-      if (!numeros.includes(randomNum) && !idUsados.includes(randomNum)) {
-        numeros.push(randomNum);
-      }
-    }
-    
-    return numeros;
-  }
-
-  elegirNumeroRandom(numeros: number[]): number {
-    const idPersonaje = Math.floor(Math.random() * numeros.length);
-    return numeros[idPersonaje];
-  }
- 
-
-
   iniciarJuego() {
-   }
+    this.usuarioJugando = true;
+    this.tiempoActual = 0;
+    this.mostrarResultado = false;
+    this.mensajeResultado = '';
+
+    // arranco el cronometro
+    this.intervalo = setInterval(() => {
+      this.tiempoActual += 0.1; // incremetno tiempo en decimas de segundos
+    }, 100);
+  }
 
   detener() {
 
-    if (this.juegoFinalizado) {
+    if (!this.usuarioJugando) {
       return; //aca voy a tener que mostrar que perdió
     }
 
-    // if (opcionElegida === this.personajeParaAdivinar.name) {
-    //   this.mensajeResultado = '¡Correcto!';
-    //   this.score += 1;
-    // } else {
-    //   this.mensajeResultado = '¡Fallaste!';
-    //   this.perderVida();
-    // } 
     this.mostrarResultado = true;
+    this.tiempoActual = parseFloat(this.tiempoActual.toFixed(1));
+    const resultadoTiempo = Math.abs(this.tiempoObjetivo - this.tiempoActual);
+    console.log('Tiempo detenido: ', this.tiempoActual, ' - Diferencia: ', resultadoTiempo);
 
+    if(resultadoTiempo == 0){
+      this.mensajeResultado = `¡Perfecto! Paraste a tiempo.`;
+      this.score += 5; 
+    } else if(resultadoTiempo < 0.5) { 
+      this.mensajeResultado = `¡Muy bien! La diferencia fue por solo ${resultadoTiempo.toFixed(1)} segundos.`;
+      this.score += 2; 
+    } else if (resultadoTiempo < 1) {
+      this.mensajeResultado = `Casi! Le erraste por ${resultadoTiempo.toFixed(1)} segundos.`;
+      this.score += 1;
+    } else { //mas de dos segundos, perdés una vida.
+      this.mensajeResultado = `¡Fallaste! Te equivocaste por ${resultadoTiempo.toFixed(1)} segundos.`;
+      this.perderVida();
+    }
 
-
-
+    if (this.vidasUsuario > 0) {
+      this.incrementarTiempoObjetivo(); // incremento el tiempo objetivo
+    }
 
     if (this.vidasUsuario === 0) {
       this.juegoFinalizado = true;
       this.mensajeResultado = '¡Te quedaste sin vidas!';
+      clearInterval(this.intervalo); // se detiene el cronometro
       return;
-    } else {
-      
-      // this.servicioPreguntas.obtenerPersonajes(this.generarNumerosRandom(this.personajesUsados)).subscribe(respuesta =>{
-      //   this.personajes = respuesta;
-      //   this.personajeParaAdivinar = this.elegirNumeroRandom(this.personajes);
-      //   console.log(this.personajes)
-        
-      // });
-    }
+    } 
   }
 
  
+  incrementarTiempoObjetivo() {
+    this.tiempoObjetivo += this.incrementoTiempoObjetivo;
+  }
 
   perderVida() {
     if (this.vidasUsuario > 0) {
@@ -100,12 +91,14 @@ export class DetenerTiempoComponent {
     this.vidasUsuario = 3;
     this.vidas = [true, true, true];
     this.score = 0;
-
+    this.tiempoObjetivo = 5;
+    this.tiempoActual = 0;
     this.mostrarResultado = false;
-    // this.servicioPreguntas.obtenerPersonajes(this.generarNumerosRandom(this.personajesUsados)).subscribe(respuesta =>{
-    //   this.personajes = respuesta;
-    //   this.personajeParaAdivinar = this.elegirNumeroRandom(this.personajes);
-    //   console.log(this.personajes)
-    // });
+    this.usuarioJugando = false;
+    this.mensajeResultado = '';
+    
+  }
+  ocultarReglas() {
+    this.mostrarReglas = false;
   }
 }
